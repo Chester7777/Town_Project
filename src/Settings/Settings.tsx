31f1dd243eed21bc1e7a0bdeced9ab72a7737a98
Es180 <Es18.03.88@gmail.com>
2021-09-03 22:20:07 +0300

    react-redux

--- src/Settings/Settings.tsx
+++ src/Settings/Settings.tsx
@@ -1,9 +1,16 @@
 import s from "./Settings.module.css";
 import {HomeOutlined} from "@ant-design/icons";
 import {Setting} from "./Settings/Setting";
+import {useState} from "react";
+import {Town} from "../Town/Town";
 
 
 export const Settings = () => {
+    // const [house, setHouse] = useState([<Town/>])
+
+    const addHouse = () => {
+        // setHouse(house)
+    }
 
     return <div className={s.app_settings}>
         <header className={s.Header_setting}>
@@ -14,7 +21,7 @@ export const Settings = () => {
         <Setting/>
         <Setting/>
         <footer className={s.Footer_setting}>
-            <button><HomeOutlined/> Build a new house</button>
+            <button onClick={addHouse} ><HomeOutlined/> Build a new house</button>
         </footer>
     </div>
 }
